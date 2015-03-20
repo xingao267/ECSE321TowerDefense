256961eafd0ab9a98546144885a8ff539ed7303d
Justin Mendonca <justin.mendonca@mail.mcgill.ca>
2015-03-20 13:04:26 -0400

    Changed MapLoader to singleton, added comments, fixed attribute visibility

--- src/Map/MapLoader.java
+++ src/Map/MapLoader.java
@@ -5,54 +5,81 @@ import com.thoughtworks.xstream.XStream;
 
 public class MapLoader {
 	
-	XStream xstream;
-	Writer writer;
+	private static MapLoader uniqueInstance = null;
 	
-	public MapLoader() {
+	private XStream xstream;
+	private Writer writer;
+
+	/**
+	 * Constructor for MapLoader
+	 */
+	private MapLoader() {
 		xstream = new XStream();
 		writer = null;
 	}
 	
-	public  void saveMap(Map m){
-				
-		String map1 = xstream.toXML(m);
+	public static synchronized MapLoader getUniqueInstance(){
+		if(uniqueInstance == null){
+			uniqueInstance = new MapLoader();
+		}
+		return uniqueInstance;
+	}
 		
+	/**
+	 * Saves the map as an XML file
+	 * 
+	 * @param m
+	 */
+	public void saveMap(Map m) {
+
+		String map1 = xstream.toXML(m);
 
 		try {
-		    writer = new BufferedWriter(new OutputStreamWriter(
-		          new FileOutputStream("map.txt"), "utf-8"));
-		    writer.write(map1);
+			writer = new BufferedWriter(new OutputStreamWriter(
+					new FileOutputStream("map.txt"), "utf-8"));
+			writer.write(map1);
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		} finally {
-		   try {writer.close();} catch (Exception ex) {}
+			try {
+				writer.close();
+			} catch (Exception ex) {
+			}
 		}
-		
+
 	}
-	
-	public  Map loadMap(String map) throws IOException{
-		
+
+	/**
+	 * Loads the XML file and turns it into a Map
+	 * 
+	 * @param map
+	 *            - name of XML file
+	 * @return Map created from XML file
+	 * @throws IOException
+	 */
+	public Map loadMap(String map) throws IOException {
+
 		map += ".txt";
 		String xml;
-		
-		BufferedReader br = new BufferedReader(new FileReader(map));
-	    try {
-	        StringBuilder sb = new StringBuilder();
-	        String line = br.readLine();
 
-	        while (line != null) {
-	            sb.append(line);
-	            sb.append(System.lineSeparator());
-	            line = br.readLine();
-	        }
-	         xml = sb.toString();
-	    } finally {
-	        br.close();
-	    }
-		
+		BufferedReader br = new BufferedReader(new FileReader(map));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append(System.lineSeparator());
+				line = br.readLine();
+			}
+			xml = sb.toString();
+		} finally {
+			br.close();
+		}
+
 		Map m = (Map) xstream.fromXML(xml);
 		return m;
-		
+
 	}
-	
+
 }
